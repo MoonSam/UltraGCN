ed60f48c44400d6612726242bc215093b8661a39
Hyunwoo_GLUCK <ty5623@naver.com>
2022-09-08 07:19:14 +0000

    change config direction

--- run_cprofile.sh
+++ run_cprofile.sh
@@ -1,1 +1,1 @@
-python -m cProfile -o profile.stats main.py --config_file config/profile_dataset_amazon.ini
+python -m cProfile -o profile.stats main.py --config_file config/profile_amazon_config.ini
--- run_memory_profile.sh
+++ run_memory_profile.sh
@@ -1,1 +1,1 @@
-python -m memory_profiler -o profile_memory.stats main.py --config_file config/profile_dataset_amazon.ini
+python -m memory_profiler -o profile_memory.stats main.py --config_file config/profile_amazon_config.ini
