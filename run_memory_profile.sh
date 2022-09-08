40fc8da4e1e85791c9a64d697ccfddd5aefa6973
Hyunwoo_GLUCK <ty5623@naver.com>
2022-09-08 10:38:31 +0000

    result of mprof

--- run_memory_profile.sh
+++ run_memory_profile.sh
@@ -1,1 +1,1 @@
-python -m memory_profiler -o profile_memory.stats main.py --config_file config/profile_amazon_config.ini
+python -m memory_profiler main.py --config_file config/profile_amazon_config.ini
---
+++ run_mprof.sh
@@ -0,0 +1,1 @@
+mprof run main.py --config_file config/profile_amazon_config.ini
