166db2643b66bb2bb9fe8efbc5d2422feaa00c26
Hyunwoo_GLUCK <ty5623@naver.com>
2022-09-09 02:26:16 +0000

    gowalla experiment 0909_1126

--- run_memory_profile.sh
+++ run_memory_profile.sh
@@ -1,1 +1,5 @@
-python -m memory_profiler main.py --config_file config/profile_amazon_config.ini
+# !/bin/bash
+
+echo Target dataset: "$1"
+
+python -m memory_profiler main.py --config_file config/profile_"$1"_config.ini
