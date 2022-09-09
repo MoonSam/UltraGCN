d0833d9e75ab559660ebce39947607082cdfa45b
Hyunwoo_Moon <hyunwoo@Hyunwoo-Moons-MacBook-Air.local>
2022-09-09 22:22:29 +0900

    Experiment setup at 0909_2222

--- run_mprof_log.sh
+++ run_mprof_log.sh
@@ -1,5 +1,21 @@
 # !/bin/bash
 
-echo Target dataset: "$1"
+echo Target dataset: electronic
 
-mprof run main.py --config_file config/profile_"$1"_config.ini
+mprof run main.py --config_file config/profile_electronic_config.ini
+
+mv *.dat results/electronic_result/
+
+echo Target dataset: m1
+
+mprof run main.py --config_file config/profile_m1_config.ini
+
+mv *.dat results/m1_result/
+
+echo Target dataset: yelp
+
+mprof run main.py --config_file config/profile_yelp_config.ini
+
+mv *.dat results/yelp_result/
+
+echo DONE!
